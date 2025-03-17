915782ca05b6c44fed3338bf86d9b44974920a4b
trandainien1 <trandainien1@gmail.com>
2025-03-17 10:43:35 +0700

    update

--- del_ins.py
+++ del_ins.py
@@ -49,7 +49,7 @@ datasets_dict = {
         'class_fn': ImageNet,
         'n_output': 1000,
         'split': 'val',
-        'indices_csv': 'datasets/2000idx_ILSVRC2012.csv',
+        'indices_csv': 'datasets/idx_ILSVRC2012.csv',
         'transform': transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((224, 224)),
