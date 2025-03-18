f8f60320568a78a616a6bb1837dfadb0e153351d
trandainien1 <trandainien1@gmail.com>
2025-03-18 14:08:33 +0700

    update

--- datasets/generate_idx.py
+++ datasets/generate_idx.py
@@ -1,6 +1,6 @@
 def generate_numbers_to_file(filename):
     with open(filename, 'w') as file:
-        for number in range(10000, 15000):
+        for number in range(20000, 25000):
             file.write(f"{number}\n")
 
 # Example usage
